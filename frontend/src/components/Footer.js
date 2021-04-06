fbc8dcea94cbf675da2d16d6c48f4454cb0d4452
Fib618 <ymst.mbl.phn@icloud.com>
2021-04-06 10:54:02 +0900

    main

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 export default class Footer extends React.Component{
     render(){
         return(
-            <h1>This is Footer Component!</h1>
+            <h1>This is Footer Component!!</h1>
         );
     }
 }
