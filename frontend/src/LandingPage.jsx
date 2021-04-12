e7174752c76a865305a605bd2fc9ad930a9036c2
Fib618 <ymst.mbl.phn@icloud.com>
2021-04-13 08:52:48 +0900

    main

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,9 +1,15 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
+import LoginForm from './components/LoginForm';
 
 class LandingPage extends Component {
-    render() {
-        return <div>Landing Page </div>;
-    }
+  render() {
+    return (
+      <div>
+        LandingPage
+        <LoginForm formText={'ログイン'} />
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
